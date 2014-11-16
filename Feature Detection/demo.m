bc8a18b7bb40ab2dd50c66eac40d1e646707c53d
chengshy <chengshangyi@gmail.com>
2014-11-16 17:43:41 -0500

    last update code before status report

--- Feature Detection/demo.m
+++ Feature Detection/demo.m
@@ -4,11 +4,14 @@ clc
 close all
 addpath('193.soccer-ball')
 addpath('017.bowling-ball')
-dirstruct=dir('193.soccer-ball\*jpg');
+addpath('088.golf-ball\')
+addpath('216.tennis-ball\')
+dirstruct=dir('216.tennis-ball\*jpg');
 L=length(dirstruct);
 center=cell(L);
 radii=cell(L);
 for i=1:L
     [center{i},radii{i}]=findCircle(dirstruct(i).name);
+    title(dirstruct(i).name);
     pause
 end 
