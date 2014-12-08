bfdbd04a3404cd6d0670598853232635eba2c42c
YaoChu <chuyao@seas.upenn.edu>
2014-12-07 22:46:13 -0500

    new PCA for train and test

--- 12.6 shangyi/PCA_train.m
+++ 12.6 shangyi/PCA_train.m
@@ -1,12 +1,15 @@
-function X_pca = reduceDimension(X, d)
+function [X_pca,coefs,mu, D] = PCA_train(X, d)
 %  Principal component analysis to reduce dimension of X
 %   X - the raw training dataset 
 %   d - the expected dimension
 
-[~,score,~]=princomp(X);
+[coefs,score,~]=princomp(X,'econ');
 % c is the number of column of X
-[~,c] = size(X); 
+[~,c1] = size(X); 
+[~,c2] = size(score); 
 % make sure that d is less or equal than c
-D = min(d,c);
+D = min(min(d,c1),c2);
 X_pca = score(:,1:D);
+
+mu = mean(X);
 end
